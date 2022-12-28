a5e2de72a7443bb2cf4e99fa649a2bab75f315f8
Adam Ulrich <a_ulrich@utb.cz>
2022-12-28 15:45:44 +0100

    add testDateTimeToMs

--- test/runtests.py
+++ test/runtests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from src.preprocessing.preprocessing import getNumberEight
-from src.preprocessing.preprocessing import CSVDateTimeToMilliseconds
+from src.preprocessing.preprocessing import CSVDateTimeToMilliseconds, apiDateTimeToMilliseconds
 from src.service.request import fetchToJson
 
 class TestPreprocessing(unittest.TestCase):
@@ -9,8 +9,11 @@ class TestPreprocessing(unittest.TestCase):
     def testGetNumberEight(self):
         self.assertEqual(getNumberEight(), 8)
 
-    def testDateTimeToMilliseconds(self):
+    def testDateTimeToMillisecondsCsv(self):
         self.assertEqual(CSVDateTimeToMilliseconds('2016-12-20 09:38:42.000'), 1482223122000.0)
+
+    def testDateTimeToMillisecondsApi(self):
+        self.assertEqual(apiDateTimeToMilliseconds('2016-12-20T09:38:42'), 1482223122000.0)
 
 
 class TestRequest(unittest.TestCase):
