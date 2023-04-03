c7934d6081b43e8ff9d07dba764156574729964d
Adam Ulrich <a_ulrich@utb.cz>
2023-04-03 15:45:57 +0200

    removed unnecessarz clusters

--- src/ai/trainer.py
+++ src/ai/trainer.py
@@ -35,44 +35,6 @@ def doAnomalyTrain(X_train, anomalyArgs, modelArgs):
     model = getattr(__import__(modelArgs["path"], fromlist=[modelArgs["what"]]), modelArgs["what"])
     return model(**anomalyArgs).fit(X_train)
 
-# 1 - not a cluster
-# 0 - is a cluster
-# SAMPLE WEIGHT - DB scan can ignore points with minus weight
-def findCluster(X_Train: Sequence[float], sample_weight, clusterArgs):
-    return findClusterDBScan(X_Train, sample_weight, **clusterArgs)
-
-
-def findClusterDBScan(X_train: Sequence[float], sample_weight, eps=7, min_samples=10):
-    return DBSCAN(eps=eps, min_samples=min_samples).fit_predict(list(X_train), sample_weight=list(sample_weight))
-
-
-def findClusterKMeans(X_train):
-    return KMeans(n_clusters=2, random_state=0, n_init="auto", algorithm="lloyd").fit_predict(X_train)
-
-
-def forestTrain(X_train, forestArgs):
-    clf = IsolationForest(**forestArgs)
-    return clf.fit(X_train)
-
-
-def svmTrain(X_train):
-    return OneClassSVM(kernel='linear', nu=0.1).fit(X_train)
-
-
-def sdgSvmTrain(X_train):
-    return SGDOneClassSVM().fit(X_train)
-
-
-def ellipticTrain(X_train):
-    return EllipticEnvelope(random_state=0, assume_centered=False, contamination=0.05).fit(X_train)
-
-
-def ellipticFitPredict(X_train):
-    return EllipticEnvelope(random_state=0, assume_centered=False, contamination=0.1).fit_predict(X_train)
-
-
-def localOutlierTrain(X_train):
-    return LocalOutlierFactor(novelty=True).fit(X_train)
 
 
 def saveModel(model, filename):
